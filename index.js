fc619d1cbc92ba14e66c879ab02381e1b14a24a9
Tudo12 <gfirman923@gmail.com>
2021-11-08 19:41:29 +0700

    Changing All code

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-// OTP Login V2 Code by Lamp
-// IDEA = Numex
-var getIP = require('external-ip')();
-const rancolor = require("randomcolor");
-var readline = require('readline');
-const {
-    Webhook,
-    MessageBuilder
-} = require('discord-webhook-node');
-const os = require("os"); 
-const user = os.userInfo().username
-var rl = readline.createInterface({ 
-    input: process.stdin, 
-    output: process.stdout 
-    });
-getIP((err, ip) => {
-    if (err) {
-        throw err;
-    }
-  
-  const Hook = new Webhook("Your Webhook URL");
-  
-  const msg = new MessageBuilder()
-                .setTitle("Login Logs")
-                .setColor(ranc)
-                .setDescription(`\`\`\`\nUsername : ${user}\nIP Address : ${ip}\nCode : ${ran}\`\`\``)
-  Hook.send(msg);
-  });
+/**
+ * @author OTP Login V3 Code by Lamp
+ * @author Remake with Tudo12
+ * @param IDEA Numex
+ */
 
-var ran = randomNum(1,9999)
-var ranc = rancolor();
+const getIP = require("external-ip")();
+const readline = require("readline");
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+const os = require("os");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function randomNum(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-ans = ran;
-rl.question(`[•] Password : `,
-(pasw)=> {
-    if(pasw.trim() == ans) {
-        console.log('Successfuly Login!')
+var ran = randomNum(1, 9999);
+function sendHook(webhookUrl) {
+  getIP((err, ip) => {
+    if (err) {
+      throw err;
     }
-    else {
-        console.log('Wrong Password!')
-        process.exit(1);
+    const hook = new Webhook(webhookUrl);
+    var today = new Date();
+    const msg = new MessageBuilder()
+      .setTitle("Login Logs")
+      .setColor("#00FF00")
+      .setDescription(`\`\`\`\nUsername : ${os.userInfo().username}\nIP Address : ${ip}\nCode : ${ran}\nDate : ${today.toLocaleString("en-GB", { timeZone: "UTC" })}\`\`\``);
+    hook.send(msg);
+  });
+}
+function quest() {
+  rl.question(`[•] Password : `, (pasw) => {
+    if (pasw.trim() == ran) {
+      console.log("Successfuly Login!");
+    } else {
+      console.log("Wrong Password!");
+      process.exit(1);
     }
-})
+    process.exit(1);
+  });
+}
+
+sendHook("YOUR_WEBHOOK_URL");
+quest();
